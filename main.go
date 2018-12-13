783aaf30c14d8e2f9a867fbe2654ad163affef20
smoqadam <phpro.ir@gmail.com>
2018-12-14 00:38:45 +0800

    Fix minor bugs Add more shortcuts Mouse click for active a cell Add status

--- main.go
+++ main.go
@@ -18,49 +18,34 @@ type Cell struct {
 type GameOfLife struct {
 	cells      [][]Cell
 	Generation int
-}
-
-type boolgen struct {
-	src       rand.Source
-	cache     int64
-	remaining int
-}
-
-func (b *boolgen) Bool() bool {
-	if b.remaining == 0 {
-		b.cache, b.remaining = b.src.Int63(), 125
-	}
-
-	result := b.cache&0x01 == 1
-	b.cache >>= 1
-	b.remaining--
-
-	return result
+	Alives     int
 }
 
 var (
 	maxRow, maxCol int
+	pause          bool
+	speed          time.Duration
+	random         bool
 )
-
-func New() *boolgen {
-	return &boolgen{src: rand.NewSource(time.Now().UnixNano())}
-}
 
 func randInt(min int, max int) int {
 	return min + rand.Intn(max-min)
 }
 
 func main() {
+	pause = true
 	err := termbox.Init()
 	if err != nil {
 		panic(err)
 	}
+	speed = 100
+	random = false
 	defer termbox.Close()
+	termbox.SetInputMode(termbox.InputMouse)
 	maxRow, maxCol = termbox.Size()
-	maxCol -= 5
+	maxCol -= 3
 	rand.Seed(time.Now().UTC().UnixNano())
-
-	b := initiate()
+	b := initiate(false)
 	eventQueue := make(chan termbox.Event)
 	go func() {
 		for {
@@ -71,31 +56,62 @@ loop:
 	for {
 		select {
 		case ev := <-eventQueue:
-			if ev.Key == termbox.KeyCtrlC {
-				break loop
+			if ev.Type == termbox.EventKey {
+				if ev.Key == termbox.KeyCtrlC {
+					break loop
+				}
+
+				if ev.Key == termbox.KeyCtrlS {
+					pause = !pause
+				}
+
+				if ev.Key == termbox.KeyCtrlN {
+					b = initiate(false)
+					pause = true
+				}
+
+				if ev.Key == termbox.KeyCtrlI {
+					speed++
+				}
+
+				if ev.Key == termbox.KeyCtrlD {
+					speed--
+				}
+
+				if ev.Key == termbox.KeyCtrlR {
+					b = initiate(true)
+				}
 			}
-			if ev.Key == termbox.KeyCtrlN {
-				b = initiate()
+			if ev.Type == termbox.EventMouse {
+				if ev.MouseX <= maxRow && ev.MouseY <= maxCol {
+					b.cells[ev.MouseY][ev.MouseX].Dead = false
+				}
 			}
-		default:
-			termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-			b.NextGen()
 			b.Print()
 			termbox.Flush()
-			time.Sleep(1 * time.Millisecond)
+
+		default:
+			if !pause {
+				b.NextGen()
+				time.Sleep(speed * time.Millisecond)
+			}
+			b.Print()
+			termbox.Flush()
 		}
 	}
 }
 
-func initiate() *GameOfLife {
+func initiate(random bool) *GameOfLife {
 	b := &GameOfLife{
 		cells: make([][]Cell, int(math.Max(float64(maxCol), float64(maxRow)))),
 	}
 	for i := 0; i <= maxCol; i++ {
 		for j := 0; j <= maxRow; j++ {
 			dead := true
-			if randInt(0, 100) < 10 {
-				dead = false
+			if random {
+				if randInt(0, 100) < 50 {
+					dead = false
+				}
 			}
 			c := Cell{
 				X:    i,
@@ -117,7 +133,7 @@ func (b *GameOfLife) NextGen() {
 		duplicate[i] = make([]Cell, len(b.cells[i]))
 		copy(duplicate[i], b.cells[i])
 	}
-
+	b.Alives = 0
 	for i := 0; i <= maxCol; i++ {
 		for j := 0; j <= maxRow; j++ {
 			ncnt := b.Nighbors(b.cells[i][j])
@@ -130,6 +146,10 @@ func (b *GameOfLife) NextGen() {
 			if ncnt == 2 {
 				duplicate[i][j] = b.cells[i][j]
 			}
+
+			if !duplicate[i][j].Dead {
+				b.Alives++
+			}
 		}
 	}
 	b.Generation++
@@ -139,11 +159,11 @@ func (b *GameOfLife) NextGen() {
 func (b *GameOfLife) Nighbors(cell Cell) int {
 	cnt := 0
 	for x1 := cell.X - 1; x1 <= cell.X+1; x1++ {
-		if x1 < 0 || x1 >= maxCol {
-			continue
-		}
 		for y1 := cell.Y - 1; y1 <= cell.Y+1; y1++ {
 			if x1 == cell.X && y1 == cell.Y {
+				continue
+			}
+			if x1 < 0 || x1 >= maxCol {
 				continue
 			}
 			if y1 < 0 || y1 >= maxRow {
@@ -158,18 +178,28 @@ func (b *GameOfLife) Nighbors(cell Cell) int {
 }
 
 func (b *GameOfLife) Print() {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+
 	for i := 0; i <= maxCol; i++ {
 		for j := 0; j <= maxRow; j++ {
-			ch := '*'
+			ch := '█'
 			if b.cells[i][j].Dead {
-				ch = ' '
+				ch = '░'
 			}
 			termbox.SetCell(j, i, ch, termbox.ColorDefault, termbox.ColorDefault)
 		}
 	}
+	b.PrintStatus()
+}
 
-	status := "Generation: " + strconv.Itoa(b.Generation)
-	for x, ch := range status {
-		termbox.SetCell(x+1, maxCol-2, ch, termbox.ColorDefault, termbox.ColorDefault)
+func (b *GameOfLife) PrintStatus() {
+	statuses := []string{
+		"Generation: " + strconv.Itoa(b.Generation) + " | Alives: " + strconv.Itoa(b.Alives) + " | Speed: " + (speed * time.Microsecond).String(),
+		"Shortcuts: New (Ctrl+N) | Start/Pause(Ctrl+S) | Random (Ctrl+R) | Set Cell Alive (Mouse Click) | Speed Up (Ctrl+D) | Speed Down (Ctrl+I)",
+	}
+	for c, status := range statuses {
+		for x, ch := range status {
+			termbox.SetCell(x+1, maxCol+c+1, ch, termbox.ColorCyan, termbox.ColorDefault)
+		}
 	}
 }
